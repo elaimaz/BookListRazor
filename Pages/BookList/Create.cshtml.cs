d835d2894b0e0b0a9717345a800d313915784d32
elaimaz <fariawesley4@gmail.com>
2020-05-13 16:55:58 -0300

    Create Book Page Model

---
+++ Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public Book Books { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
