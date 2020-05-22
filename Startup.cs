de10d9bdc560542a18fe6413f94f683a6d8b2044
elaimaz <fariawesley4@gmail.com>
2020-05-22 17:48:30 -0300

    Book Get API

--- Startup.cs
+++ Startup.cs
@@ -21,6 +21,7 @@ namespace BookListRazor
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllersWithViews();
             services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
@@ -47,6 +48,7 @@ namespace BookListRazor
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
                 endpoints.MapRazorPages();
             });
         }
