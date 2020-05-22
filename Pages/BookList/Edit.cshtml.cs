eb6b8091380eb87bb914779bdfa74897c5688402
elaimaz <fariawesley4@gmail.com>
2020-05-22 16:26:50 -0300

    Edit Post Handler

--- Pages/BookList/Edit.cshtml.cs
+++ Pages/BookList/Edit.cshtml.cs
@@ -21,5 +21,21 @@ namespace BookListRazor.Pages.BookList
         {
             Book = await _db.Book.FindAsync(id);
         }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var bookFromDb = await _db.Book.FindAsync(Book.Id);
+                bookFromDb.Name = Book.Name;
+                bookFromDb.Author = Book.Author;
+                bookFromDb.ISBN = Book.ISBN;
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            return RedirectToPage();
+        }
     }
 }
