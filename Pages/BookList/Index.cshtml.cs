308e27dff35d7222e80a2a716705272ac537fb58
elaimaz <fariawesley4@gmail.com>
2020-05-22 16:53:36 -0300

    Delete Book

--- Pages/BookList/Index.cshtml.cs
+++ Pages/BookList/Index.cshtml.cs
@@ -1,4 +1,5 @@
 ﻿using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
@@ -20,6 +21,19 @@ namespace BookListRazor.Pages.BookList
         public async Task OnGet()
         {
             Books = await _db.Book.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await _db.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
         }
     }
 }
