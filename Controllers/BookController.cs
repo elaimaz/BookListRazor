89242bcdb3fce53e2ad73c17b0c0d441398291ed
elaimaz <fariawesley4@gmail.com>
2020-05-23 14:24:42 -0300

    Delete API Call

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,6 +1,8 @@
 ﻿using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace BookListRazor.Controllers
 {
@@ -16,9 +18,22 @@ namespace BookListRazor.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            return Json(new { data=_db.Book.ToList()});
+            return Json(new { data= await _db.Book.ToListAsync()});
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error While Deleting"});
+            }
+            _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success=true, message="Delete Successful"});
         }
     }
 }
